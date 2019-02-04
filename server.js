e51a0c3d37be09450543eba1ff83a4db474f73ad
Raj <rajdeep.sharma17@gmail.com>
2019-02-04 11:56:55 +0530

    add: json route

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ var app = express()
 app.use(express.static(__dirname + '/dist'));
 
 
+var data = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+app.get('/json', function(req,res) {
+    res.json(data);
+});
+
 app.get('/*', function(req,res) {
     res.sendFile(__dirname+'/dist/mypr/index.html');
 });
+
+
 
 app.listen(process.env.PORT || 3000);
 
